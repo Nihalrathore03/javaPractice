8eb283287194a4904691978f27180f912005fc27
Nihalrathore03 <nihalrathore0207@gmail.com>
2025-01-01 17:20:00 +0000

    Add files via upload

---
+++ PrintTriangle.java
@@ -0,0 +1,10 @@
+public class PrintTriangle {
+    
+    public static void main(String[] args) {
+        for(int i =1;i<5;i++){
+            for(int n=1; n<10;n++){
+                System.out.print("*");
+            }
+        }System.out.println("\n");
+    }
+}
